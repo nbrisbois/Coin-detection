ce81ac83efa5f23427d96a730c6ddc5316dcc8de
Nikolas <33031353+nbrisbois@users.noreply.github.com>
2019-11-24 16:34:01 -0500

    Better update
    this should actually do something

--- Coin-detection.m
+++ Coin-detection.m
@@ -3,31 +3,31 @@ I = lin2rgb(I);
 I = imresize(I, 0.25);
 
 I = imcomplement(I);
-imshow(I);
-
+numPen = 0;
+numOfCoin = size(centers);
+penLoc = zeros(numOfCoin, 2, 'uint8');
+UnId = penLoc;
 [centers, radii] = imfindcircles(I, [10 100]);
-numCoin = size(centers);
-DimCou = zero(numCoin, 2);
-PenCou = zero(numCoin, 2);
-NicCou = zero(numCoin, 2);
-QuaCou = zero(numCoin, 2);
-dim = 0;
-pen = 0;
-nic = 0;
-qua = 0;
-max = 20;
-min = 10;
-magicFour = 0
-for r = 0: 100
-    [centers, radii] = imfindcircles(I, [min max]);
-    if(centers == 0) 
-        max = round(max/.8);
-        min = round(min/.9);
-    end
-    
-            
-end
-viscircles(centers, radii, 'EdgeColor', 'r');
 
-function result = isPen(center, radii)
+for c = 1: numOfCoin
+    corner = round(radii(c)/2);
+    cop = 0;
+    for i = centers(c, 1) - corner:centers(c, 1) + corner
+        for j = centers(c, 2) - corner: centers(c, 2) + corner
+            if(I(i,j,1) > 68 && I(i,j,1) < 76 && I(i,j,2) > 41 && I(i,j,2) < 49)
+                cop = cop + 1;
+            end
+        end
+    end
+    if(cop/3)
+        numPen = numPen + 1;
+        penLoc(numPen, 1) = centers(numPen, 1);
+        penLoc(numPen, 2) = centers(numPen, 2);
+    else
+        penLoc(c, 1) = centers(c, 1);
+        penLoc(c, 2) = centers(c, 2);
+    end
 end
+I = imcomplement(I);
+imshow(I);
+viscircles(centers, radii, 'EdgeColor', 'r');
